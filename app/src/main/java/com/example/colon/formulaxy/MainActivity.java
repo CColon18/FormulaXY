27fbab0a3758b2fcd85aa6101d0e1795bd5986ee
LastChanceBilly <josetecnom12@hotmail.com>
2018-11-20 00:00:14 -0500

    Error tvParrafo arreglado (Declarado tvParrafo y ij al principio de clase Mainactivity)

--- app/src/main/java/com/example/colon/formulaxy/MainActivity.java
+++ app/src/main/java/com/example/colon/formulaxy/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -13,6 +14,8 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
+    InfoJava ij = new InfoJava();
+    TextView tvParrafo = ij.tvParrafo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
